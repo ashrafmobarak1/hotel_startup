cd92a26d01f45c856378c25e1cea89949012cc2c
ashrafmobarak1 <ashrafmobarak1@gmail.com>
2024-09-21 18:14:14 +0300

    initiate docker files

--- obj/Debug/netcoreapp3.1/hotel_startup.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/hotel_startup.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hotel_startup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f0f67f3799daadb008477702108d1d81475e0a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("hotel_startup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hotel_startup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
